e02eb45e848aa2275da19ce32ee5eb4477a8c25d
GB11 <mypokupka@ukr.net>
2025-06-10 19:49:22 +0300

    Update __init__.py

--- components/pipsolar/sensor/__init__.py
+++ components/pipsolar/sensor/__init__.py
@@ -309,16 +309,7 @@ TYPES = {
         accuracy_decimals=1,
         device_class=DEVICE_CLASS_POWER,
     ),
-    CONF_PV_ENERGY_TODAY: sensor.sensor_schema(
-        unit_of_measurement=UNIT_WATT_HOUR,
-        accuracy_decimals=1,
-        device_class=DEVICE_CLASS_ENERGY,        
-    ),
-    CONF_PV_ENERGY_TOTAL: sensor.sensor_schema(
-        unit_of_measurement=UNIT_WATT_HOUR,
-        accuracy_decimals=1,
-        device_class=DEVICE_CLASS_ENERGY,        
-    ),
+    
 }
 
 CONFIG_SCHEMA = PIPSOLAR_COMPONENT_SCHEMA.extend(
