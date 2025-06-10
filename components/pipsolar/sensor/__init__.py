6a6097206f6a1467bf4a1cfb382615a2171c3eae
GB11 <mypokupka@ukr.net>
2025-06-10 20:01:37 +0300

    Update __init__.py

--- components/pipsolar/sensor/__init__.py
+++ components/pipsolar/sensor/__init__.py
@@ -309,6 +309,11 @@ TYPES = {
         accuracy_decimals=1,
         device_class=DEVICE_CLASS_POWER,
     ),
+    CONF_PV_ENERGY_TODAY: sensor.sensor_schema(
+        unit_of_measurement=UNIT_WATT,
+        accuracy_decimals=1,
+        device_class=DEVICE_CLASS_POWER,
+    ),
     
 }
 
