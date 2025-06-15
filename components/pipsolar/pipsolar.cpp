99afbddb6cc2ab6a62dd15c0cb6629b6003d2af6
GB11 <mypokupka@ukr.net>
2025-06-15 22:44:42 +0300

    Update pipsolar.cpp

--- components/pipsolar/pipsolar.cpp
+++ components/pipsolar/pipsolar.cpp
@@ -157,13 +157,13 @@ void Pipsolar::loop() {
         }
         // Test enviroment
         if (this->charger_source_priority_solar_first_switch_) {
-          this->charger_source_priority_solar_first_switch_->publish_state(charger_source_priority_);
+          this->charger_source_priority_solar_first_switch_->publish_state(charger_source_priority_ == 1);
         }
         if (this->charger_source_priority_solar_utility_switch_) {
-          this->charger_source_priority_solar_utility_switch_->publish_state(charger_source_priority_);
+          this->charger_source_priority_solar_utility_switch_->publish_state(charger_source_priority_ == 2);
         }
         if (this->charger_source_priority_solar_only_switch_) {
-          this->charger_source_priority_solar_only_switch_->publish_state(charger_source_priority_);
+          this->charger_source_priority_solar_only_switch_->publish_state(charger_source_priority_ == 3);
         }
             
         // special for charger source priority select
