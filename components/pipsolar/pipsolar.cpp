edd7b9bbf90efe64333f7c959afad25b52849c52
GB11 <mypokupka@ukr.net>
2025-06-30 17:20:09 +0300

    Update pipsolar.cpp

--- components/pipsolar/pipsolar.cpp
+++ components/pipsolar/pipsolar.cpp
@@ -491,6 +491,8 @@ void Pipsolar::loop() {
         this->state_ = STATE_IDLE;
         break;   
       case POLLING_QT:
+      case POLLING_Q1:
+      case POLLING_Q2:
       case POLLING_QMN:
         this->state_ = STATE_IDLE;
         break;
@@ -774,6 +776,20 @@ void Pipsolar::loop() {
         ESP_LOGD(TAG, "Decode QT");
         if (this->last_qt_) {
           this->last_qt_->publish_state(tmp);
+        }
+        this->state_ = STATE_POLL_DECODED;
+        break;
+      case POLLING_Q1:
+        ESP_LOGD(TAG, "Decode Q1");
+        if (this->last_q1_) {
+          this->last_q1_->publish_state(tmp);
+        }
+        this->state_ = STATE_POLL_DECODED;
+        break;
+      case POLLING_Q2:
+        ESP_LOGD(TAG, "Decode Q2");
+        if (this->last_q2_) {
+          this->last_q2_->publish_state(tmp);
         }
         this->state_ = STATE_POLL_DECODED;
         break;
