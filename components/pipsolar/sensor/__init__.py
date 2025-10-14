9074726230c4f7feeb00af3a4b20b244959ca8e5
GB11 <mypokupka@ukr.net>
2025-10-14 14:34:06 +0300

    Update __init__.py

--- components/pipsolar/sensor/__init__.py
+++ components/pipsolar/sensor/__init__.py
@@ -319,11 +319,6 @@ TYPES = {
         accuracy_decimals=0,
         device_class=DEVICE_CLASS_ENERGY,
         state_class=STATE_CLASS_TOTAL_INCREASING,
-    
- #   CONF_PV_ENERGY_TODAY: sensor.sensor_schema(
- #       unit_of_measurement=UNIT_WATT,
- #       accuracy_decimals=1,
- #       device_class=DEVICE_CLASS_POWER,
     ),
     CONF_PV_ENERGY_TOTAL: sensor.sensor_schema(
         unit_of_measurement=UNIT_WATT_HOURS,
@@ -331,11 +326,6 @@ TYPES = {
         accuracy_decimals=0,
         device_class=DEVICE_CLASS_ENERGY,
         state_class=STATE_CLASS_TOTAL_INCREASING,
-    
-  #  CONF_PV_ENERGY_TOTAL: sensor.sensor_schema(
-  #      unit_of_measurement=UNIT_WATT,
-  #      accuracy_decimals=1,
-  #      device_class=DEVICE_CLASS_POWER,
     ),
     
 }
