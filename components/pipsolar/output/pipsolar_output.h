25da56f767a0dfe1f24adb08f7ec7b8f63cc7632
GB11 <mypokupka@ukr.net>
2025-12-14 14:29:44 +0200

    Add automation and version includes to pipsolar_output.h

--- components/pipsolar/output/pipsolar_output.h
+++ components/pipsolar/output/pipsolar_output.h
@@ -3,6 +3,8 @@
 #include "../pipsolar.h"
 #include "esphome/components/output/float_output.h"
 #include "esphome/core/component.h"
+#include "esphome/core/automation.h"
+#include "esphome/core/version.h"
 
 namespace esphome {
 namespace pipsolar {
@@ -29,8 +31,11 @@ template<typename... Ts> class SetOutputAction : public Action<Ts...> {
   SetOutputAction(PipsolarOutput *output) : output_(output) {}
 
   TEMPLATABLE_VALUE(float, level)
-
-  void play(Ts... x) override { this->output_->set_value(this->level_.value(x...)); }
+#if ESPHOME_VERSION_CODE >= VERSION_CODE(2025, 11, 0)
+  void play(const Ts &...x) override { this->output_->set_value(this->level_.value(x...)); }
+#else
+    void play(Ts... x) override { this->output_->set_value(this->level_.value(x...)); }
+#endif
 
  protected:
   PipsolarOutput *output_;
